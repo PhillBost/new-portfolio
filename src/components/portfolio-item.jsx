11422d3972c84cc1798754ee92c28724e8160635
Phillip B <pbostic789@gmail.com>
2025-07-03 12:53:27 -0400

    installed prettier + portfolio item

--- src/components/portfolio-item.jsx
+++ src/components/portfolio-item.jsx
@@ -1,14 +1,15 @@
-function portfolio_item({projectsrc, projectdesc}) {
+function PortfolioItem({ projectsrc, projectdesc }) {
     return (
-        <div style={{backgroundImage: `url(${projectsrc})`}} 
-            class="h-50 w-1/2 sm:w-1/3 ">
-
-            <div 
-                class={"text-center "}>
-                {projectdesc}
-            </div>
+      <div
+        className="h-[300px] w-[33vw] sm:w-[50vw] bg-no-repeat bg-[length:100%] hover:opacity-65 hover:bg-[length:120%] hover:cursor-pointer bg-center transition-[background-size] duration-400 ease-in-out"
+        style={{ backgroundImage: `url(${projectsrc})` }}
+      >
+        <div className="">
+          {projectdesc}
         </div>
-    )
-}
-
-export default portfolio_item;
+      </div>
+    );
+  }
+  
+  export default PortfolioItem;
+  
